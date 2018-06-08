06b9e0a0b398abc5ccf8c5db9371c393ccf90987
Sergey <sergm005@wase-am06001729.swna.wdpr.disney.com>
2018-06-08 14:22:37 -0700

    updated main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+var targetNode = document.getElementById('content');
+
+var mutationObserver = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+//      console.log(mutation);
+    switch(mutation.target.nodeName) {
+        case "text":
+            console.log(mutation); 
+            break;
+            
+        default: 
+            break;
+    }
+    
+  });
+});
+
+// Starts listening for changes in the root HTML element of the page.
+mutationObserver.observe(targetNode, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
+});
