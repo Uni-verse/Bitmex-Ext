7fb5888ee29f7e51ec1c74d0bda93fa664f87946
Uni-verse <sergm005@wase-am06001729.swna.wdpr.disney.com>
2018-07-13 11:36:11 -0700

    Color coded highlighted trades

--- main.js
+++ main.js
@@ -1,38 +1,77 @@
-var targetNode = document.getElementById('content');
+var bitmext = (function(){
 
-var mutationObserver = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
+    //Selectors
+    var result1 = $('#result');
+    var slider1 = $("input[type='text']");
+    slider1.on("keyup", display1, false);
 
-    switch(mutation.target.className) {
-        case "highlightWrapper":
-            var c = mutation.target.children;
-            
-            for (var i = 0; i < c.length; i++) {
-                var limit = c[1].innerText;
-                limit = limit.replace(',', '');
-                if(Number(limit) > 99999){
-//                    Enable for logging mutation
-//                    console.log(mutation);
-//                    console.log(limit);
-                    mutation.target.style.backgroundColor = "Blue";
-                    mutation.target.style.color = "white";
-                    break;
-                    } 
+    //Method to display result
+    var display1 = function(e){
+        result1.text(e.target.innerText);
+        instance.init.x = Number(e.target.innerText);
+    };
+
+    //Init Method
+    function init(){
+        // x represents the size to be highlighted
+        var x = 99999;
+        var mutationObserver = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+        
+                switch(mutation.target.className) {
+                    case "highlightWrapper":
+                        var c = mutation.target.children;
+                        
+                        for (var i = 0; i < c.length; i++) {
+                            var limit = c[1].innerText;
+                            limit = limit.replace(',', '');
+                            if(Number(limit) > x){
+                                if (c[3].innerText == "S"){
+                                    mutation.target.style.backgroundColor = "Red";
+                                    mutation.target.style.color = "white";
+                                }else{
+                                    mutation.target.style.backgroundColor = "Green";
+                                    mutation.target.style.color = "white";
+                                }
+            //                    Enable for logging mutation
+            //                    console.log(mutation);
+            //                    console.log(limit);
+                                
+                                // playSound();
+                                break;
+                                } 
+                            }
+            //            console.log(mutation);
+                    default: 
+                        break;
                 }
-//            console.log(mutation);
-        default: 
-            break;
-    }
-    
-  });
-});
+                
+            });
+        });
+        return mutationObserver;
+    };
+    return {
+        init: init
+    };
+})();
 
-// Starts listening for changes in the root HTML element of the page.
-mutationObserver.observe(targetNode, {
-  attributes: true,
-  characterData: true,
-  childList: true,
-  subtree: true,
-  attributeOldValue: true,
-  characterDataOldValue: true
-});
+//Start when document loads
+$(document).ready(function(){
+
+    //invoke init, instantiate MutationObserver
+    //Assign to instance var
+    var instance = bitmext.init();
+
+    //Select Node to be scanned for mutations
+    var targetNode = document.getElementById('content');
+
+    //Begin mutation scans
+    instance.observe(targetNode, {
+        attributes: true,
+        characterData: true,
+        childList: true,
+        subtree: true,
+        attributeOldValue: true,
+        characterDataOldValue: true
+        });
+}); 
